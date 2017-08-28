dfb5b8fd20c34ae25ded79c51d3e9f0ab769a420
sudeepgalgali <17sg17@gmail.com>
2017-08-27 22:59:11 -0600

    Update mini_interpreter.py

--- mini_interpreter.py
+++ mini_interpreter.py
@@ -1,22 +1,26 @@
+#importing required packages
 import ply.lex as lex
 import sys
 import ply.yacc as yacc
 
+#initializing reserved words, tokens and literals for the lexical analyzer
 reserved = {'print' : 'PRINT'}
 tokens= ['VAR','NUMBER','PLUS','MINUS','MUL','DIV',] + list(reserved.values())
 literals= ['+','=','-','*','/']
 
+#making sure a reserved word is not used as a variable name
 def t_VAR(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     t.type = reserved.get(t.value,'VAR')
     return t
 
-#match numbers and convert to float
+#match numbers and convert to integer
 def t_NUMBER(t):
     r'\d+'
-    t.value=float(t.value)
+    t.value=int(t.value)
     return t
 
+#ignoring tabspaces(whitespaces are automatically ignored)
 t_ignore=" \t"
 
 #counting line numbers, needed for effective debugging
@@ -31,11 +35,12 @@ def t_error(t):
 #intialize lexer
 lex.lex(debug=1)
 
-#define precedence
+#define precedence(this is needed to resolve ambiguity in the case where the grammar is not rewritten to remove it explicitly)
 precedence =(('left','*'),('left','/'),('left','+'),('left','-'))
 
 var={}
 
+#defining the rules of the grammar
 def p_equation(p):
     'S : VAR "=" EXP'
     var[p[1]] = p[3]
@@ -78,7 +83,7 @@ def p_error(p):
     else:
         print("Syntax Error at EOF")
         
-
+#initializing yacc
 yacc.yacc()
 fp = open(sys.argv[1],"r")
 ll=fp.readlines()
