2d03cca7bf6e1f53707b176aeb122bfde4803128
sudeepgalgali <17sg17@gmail.com>
2017-08-27 18:09:00 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,7 @@
-import sys
+imposample_textsys
+print "Test 1, with ex1.py"
 sys.argv = ['mini_interpreter.py','ex1.py']
+execfile('mini_interpreter.py')
+print "Test 2, with all arithmetic operations (sample_inputs)"
+sys.argv = ['mini_interpreter.py','sample_inputs']
 execfile('mini_interpreter.py')
